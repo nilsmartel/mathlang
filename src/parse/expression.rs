146dc994a60388fb5b33892803dc7675962da64a
Nils Martel <nils.martel@graphmasters.net>
2020-08-25 14:59:24 +0200

    More tests

--- src/parse/expression.rs
+++ src/parse/expression.rs
@@ -5,6 +5,7 @@ use super::{
 };
 use nom::{character::complete::char, IResult};
 
+#[derive(Clone, Eq, PartialEq, Debug)]
 pub enum Expression {
     Add(Box<Expression>, Box<Expression>),
     Subtract(Box<Expression>, Box<Expression>),
@@ -20,6 +21,12 @@ pub enum Expression {
         parameters: Vec<Expression>,
     },
     Number(Number),
+}
+
+impl From<&str> for Expression {
+    fn from(i: &str) -> Self {
+        Expression::parse(i).expect("failed to parse Expression").1
+    }
 }
 
 impl Expression {
@@ -115,4 +122,48 @@ fn literal(i: &str) -> IResult<&str, Expression> {
 fn number(i: &str) -> IResult<&str, Expression> {
     use nom::combinator::map;
     map(Number::parse, Expression::Number)(i)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::Parse;
+    use super::*;
+    #[test]
+    fn expression() {
+        fn test(input: &str, expected: Expression) {
+            assert_eq!(Expression::parse(input), Ok(("", expected)))
+        }
+
+        use Expression::*;
+        test(
+            "sin",
+            Literal {
+                identifier: "sin".into(),
+                parameters: Vec::new(),
+            },
+        );
+        test(
+            "a+b",
+            Add(Expression::from("a").boxed(), Expression::from("b").boxed()),
+        );
+        test(
+            "a + b",
+            Add(Expression::from("a").boxed(), Expression::from("b").boxed()),
+        );
+        test(
+            "a^2 + b^2",
+            Add(
+                Power(
+                    Expression::from("a").boxed(),
+                    Expression::Number(2.0.into()).boxed(),
+                )
+                .boxed(),
+                Power(
+                    Expression::from("b").boxed(),
+                    Expression::Number(2.0.into()).boxed(),
+                )
+                .boxed(),
+            ),
+        )
+    }
 }

b08f341885a3554606c8788945b1660ba70b7559
Nils Martel <nils.martel@graphmasters.net>
2021-02-20 12:48:37 +0100

    Include handy interface to parse expressions

--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -1,6 +1,9 @@
-pub mod atomic;
-pub mod expression;
+mod atomic;
+mod expression;
 mod util;
+
+pub use atomic::*;
+pub use expression::*;
 
 pub(crate) trait Parse
 where
@@ -11,4 +14,17 @@ where
     fn parse_ws(input: &str) -> nom::IResult<&str, Self> {
         util::skip_ws(Self::parse)(input)
     }
+}
+
+pub fn parse_expression_ast(input: &str) -> Option<Expression> {
+    match Expression::parse_ws(input) {
+        Ok((rest, ast)) if is_all_whitespace(rest) => Some(ast),
+        _ => None,
+    }
+}
+
+fn is_all_whitespace(i: &str) -> bool {
+    let (rest, _) = util::whitespace(i).unwrap();
+
+    rest == ""
 }
